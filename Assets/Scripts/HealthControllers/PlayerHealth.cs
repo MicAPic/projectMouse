e41cb7087732bcdafad34ce71ef8dba63e5ff8f9
Michael Pavlov <micha.pavlov@gmail.com>
2023-08-16 21:11:37 +0300

    Add a health bar mock-up

--- Assets/Scripts/HealthControllers/PlayerHealth.cs
+++ Assets/Scripts/HealthControllers/PlayerHealth.cs
@@ -1,11 +1,21 @@
 ﻿using CameraShake;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace HealthControllers
 {
     public class PlayerHealth : Health
     {
+        [Header("HP")]
         public float defenceModifier = 1.0f;
         public float MaxHealth { get; private set; }
+
+        [Header("Visuals")]
+        [SerializeField] 
+        private Image healthBar;
+        [SerializeField] 
+        private float healthBarChangeDuration;
 
         // Start is called before the first frame update
         void Start()
@@ -16,22 +26,30 @@ namespace HealthControllers
         public void FullHeal()
         {
             healthPoints = MaxHealth;
+            UpdateHealthBar();
         }
 
         public void IncreaseMaxHealth(float increment)
         {
             MaxHealth += increment;
+            UpdateHealthBar();
         }
         
         public override void TakeDamage(float damagePoints)
         {
             base.TakeDamage(damagePoints * defenceModifier);
             CameraShaker.Presets.Explosion2D(rotationStrength:0.1f);
+            UpdateHealthBar();
         }
 
         protected override void Die() 
         {
             GameManager.Instance.GameOver();
+        }
+
+        private void UpdateHealthBar()
+        {
+            healthBar.DOFillAmount(healthPoints / MaxHealth, healthBarChangeDuration);
         }
     }
 }
