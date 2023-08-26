481688c3cffea2662a06d33ad054cd60ec386e64
GeorgeD615 <st095277@student.spbu.ru>
2023-08-26 18:46:05 +0300

    addMachineGunEnemy

--- Assets/Scripts/Enemy/EnemyWithMagicController.cs
+++ Assets/Scripts/Enemy/EnemyWithMagicController.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+
+
 
 namespace Enemy
 {
@@ -23,23 +26,61 @@ namespace Enemy
 
         private void SetUpBullets()
         {
-            _mainRotationVector = Vector3.up * _radius;
+            //_mainRotationVector = Vector3.up * _radius;
             for (int i = 0; i < _bullets.Capacity; ++i)
             {
-                Vector3 offset = Quaternion.Euler(0,0, (360 / _numberOfBullets) * i) * _mainRotationVector;
+                //Vector3 offset = Quaternion.Euler(0,0, (360 / _numberOfBullets) * i) * _mainRotationVector;
                 _bullets[i] = BulletPool.Instance.GetBulletFromPool(1);
-                _bullets[i].transform.position = transform.position + offset;
+                _bullets[i].transform.position = _shootingPoint.position/* + offset*/;
                 _bullets[i].EnableWithoutForce(damageToDeal);
             }
+            _bulletsAnimFinished = false;
+            StartCoroutine(AnimateBulletsInst());
         }
-        private void RotateBullets()
+
+
+        private float _timeFromStartBulletAnim = 0;
+        private bool _bulletsAnimFinished;
+        private float _animTimeScaler = 3;
+        private IEnumerator AnimateBulletsInst()
         {
-            _mainRotationVector = Quaternion.AngleAxis(_bulletsRotationSpeed*Time.deltaTime, Vector3.forward) * _mainRotationVector;
+            _mainRotationVector = Vector3.up * _radius;
+            while(_timeFromStartBulletAnim / _animTimeScaler < 1)
+            {
+                for (int i = 0; i < _bullets.Capacity; ++i)
+                {
+                    Vector3 offset = Quaternion.Euler(0, 0, (360 / _numberOfBullets) * i) * _mainRotationVector;
+                    if (_bullets[i] != null) 
+                        _bullets[i].transform.position = Vector3.Lerp(_shootingPoint.position, _shootingPoint.position + offset, _timeFromStartBulletAnim / _animTimeScaler);
+                    _timeFromStartBulletAnim += Time.deltaTime;
+                }
+                yield return null;
+            }
+
+
             for (int i = 0; i < _bullets.Capacity; ++i)
             {
                 Vector3 offset = Quaternion.Euler(0, 0, (360 / _numberOfBullets) * i) * _mainRotationVector;
                 if(_bullets[i] != null)
-                    _bullets[i].transform.position = transform.position + offset;
+                    _bullets[i].transform.position = _shootingPoint.position + offset;
+            }
+
+            _bulletsAnimFinished = true;
+            _timeFromStartBulletAnim = 0;
+
+        }
+
+        private void RotateBullets()
+        {
+            if (_bulletsAnimFinished)
+            {
+                _mainRotationVector = Quaternion.AngleAxis(_bulletsRotationSpeed * Time.deltaTime, Vector3.forward) * _mainRotationVector;
+                for (int i = 0; i < _bullets.Capacity; ++i)
+                {
+                    Vector3 offset = Quaternion.Euler(0, 0, (360 / _numberOfBullets) * i) * _mainRotationVector;
+                    if (_bullets[i] != null)
+                        _bullets[i].transform.position = transform.position + offset;
+                }
             }
         }
 
