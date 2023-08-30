2c2578920849157b866f35bba398b6a2b5684fa9
GeorgeD615 <st095277@student.spbu.ru>
2023-08-30 22:29:51 +0300

    add new PowerUps balance

--- Assets/Scripts/PowerUps/MagicBulletsPowerUp.cs
+++ Assets/Scripts/PowerUps/MagicBulletsPowerUp.cs
@@ -1,10 +1,19 @@
+using UnityEngine;
 namespace PowerUps
 {
     public class MagicBulletsPowerUp : PowerUpBase
     {
+        [SerializeField] private ShotGunPowerUp _shotGunPowerUp;
         protected override void Activate()
         {
             PlayerController.Instance.EnableMagicBullets();
+            var powerUpName = _shotGunPowerUp.GetType().Name;
+            foreach (var pair in ExperienceManager.Instance.PowerUpsWithCounters)
+            {
+                if (!pair.Key.name.Contains(powerUpName)) continue;
+                ExperienceManager.Instance.PowerUpsWithCounters[pair.Key] += 1;
+                break;
+            }
             ExperienceManager.Instance.RemoveFromPowerUps(this);
         }
     }
--- Assets/Scripts/PowerUps/ShotGunPowerUp.cs
+++ Assets/Scripts/PowerUps/ShotGunPowerUp.cs
@@ -1,10 +1,19 @@
+using UnityEngine;
 namespace PowerUps
 {
     public class ShotGunPowerUp : PowerUpBase
     {
+        [SerializeField] private MagicBulletsPowerUp _magicBulletsPowerUp;
         protected override void Activate()
         {
             PlayerController.Instance.EnableShotgun();
+            var powerUpName = _magicBulletsPowerUp.GetType().Name;
+            foreach (var pair in ExperienceManager.Instance.PowerUpsWithCounters)
+            {
+                if (!pair.Key.name.Contains(powerUpName)) continue;
+                ExperienceManager.Instance.PowerUpsWithCounters[pair.Key] += 1;
+                break;
+            }
             ExperienceManager.Instance.RemoveFromPowerUps(this);
         }
     }
