d0f7e357a80f6fe5c2380ed15132e93986f784af
Michael Pavlov <micha.pavlov@gmail.com>
2023-09-08 00:06:41 +0300

    Add end-game statistics

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -1,4 +1,5 @@
 using Dan.Main;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,6 +23,10 @@ namespace UI
         private GameObject offlineSign;
         [SerializeField]
         private LeaderboardEntry[] leaderboardEntries;
+        [SerializeField]
+        private CanvasGroup statistics;
+        [SerializeField]
+        private TMP_Text statisticsPopUp;
 
         protected virtual void Start()
         {
@@ -46,7 +51,7 @@ namespace UI
             buttons[1].Select();
         }
 
-        public void UpdateLeaderboardContent(string publicKey)
+        public void UpdateLeaderboardContent(string publicKey, int score=0)
         {
             LeaderboardCreator.GetLeaderboard(publicKey, entries =>
             {
@@ -57,6 +62,28 @@ namespace UI
                 for (var i = 0; i < numberOfEntries; i++)
                 {
                     leaderboardEntries[i].PopulateEntryFields(entries[i].Username, entries[i].Score);
+                }
+                
+                if (score == 0) return;
+                
+                // Calculate statistics
+                var noOfPeople = 0;
+                for (var j = entries.Length - 1; j >= 0; j--)
+                {
+                    Debug.Log(entries[j].Score);
+                    if (score >= entries[j].Score)
+                    {
+                        noOfPeople++;
+                    }
+                    else
+                    {
+                        // statistics.DOFade(1.0f, 0.5f).SetUpdate(true);
+                        statisticsPopUp.DOCounter(
+                            0, 
+                            Mathf.RoundToInt(noOfPeople / (float)entries.Length * 100.0f), 
+                            1.0f).SetUpdate(true).SetEase(Ease.OutCirc);
+                        break;
+                    }
                 }
             });
         }
